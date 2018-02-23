6124c685a55f479529739ba6c02506c9c36a0091
Ryan Parke <ryanjparke94@gmail.com>
2018-02-23 16:45:48 -0700

    slide out menu complete

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import App from './App';
 import { Provider } from 'react-redux'; //becomes aware of state / store
 // import store from './store';
 import { unregister } from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+
+const Material = () => (
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+)
 
 ReactDOM.render(
     <Provider  >
-<App />
+<Material />
 </Provider>,
  document.getElementById('root'));
 unregister();
