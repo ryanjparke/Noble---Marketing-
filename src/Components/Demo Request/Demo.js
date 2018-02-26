ccf412ea284dfb49c51ed79a503b7da1da5c2bfc
Ryan Parke <ryanjparke94@gmail.com>
2018-02-26 16:58:09 -0700

    demo request added

--- src/Components/Demo Request/Demo.js
+++ src/Components/Demo Request/Demo.js
@@ -1,2 +1,27 @@
 //nodemailer
 //material ui
+import React, { Component } from 'react';
+import './Demo.css';
+import { Link } from 'react-router-dom';
+
+
+
+class Demo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className='demoContainer'>
+                <p>Request a demo</p>
+                </div>
+                )
+    }
+}
+
+export default Demo;
