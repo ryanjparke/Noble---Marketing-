302532bea46823abbd521af4c1be76bd9097be3c
Ryan Parke <ryanjparke94@gmail.com>
2018-03-09 14:11:11 -0700

    home page edits and demo req edits

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 
 import Whiteboard from './../../assets/Whiteboard.mp4';
 import Noblewhite from './../../assets/Noblewhite.png';
+import Mountains from './../../assets/Mountains.jpg';
 
 
 class Home extends Component {
@@ -22,16 +23,14 @@ class Home extends Component {
     // }
     render() {
         return (
-            <div className='HomeContainer'>
-                {/* ----------FONT URL---------- */}
-                {/* <style>@import url('https://cloud.typography.com/7268774/6354952/css/fonts.css');</style> */}
+            <div className='homeContainer'>
                 <Nav />
-                <div className='backgroundVid' >
-                    <video loop autoPlay><source src={Whiteboard} type="video/mp4" />
-                    </video>
-                    <div className='demoButton' >
-                    <a href='/demo'>Request a demo </a>
-                    </div>
+                <div className='welcomeBox'>
+                    <div className='innerWelcome'>
+                    <img src={Noblewhite} className='nobleWhite'/>
+                <h1>Assessments. Case Planning. Easy.</h1>
+                <a href='/demo'><h4 className='getStarted'>Get started today</h4> </a>
+                </div>
                 </div>
 
 
@@ -43,11 +42,21 @@ class Home extends Component {
 
                 {/* MOBILE VIEW */}
                 <div className='mobileHome'>
+                    <Nav />
 
-            </div >
-                {/* Container Div */ }
                 </div >
-                )
+                {/* Container Div */}
+            </div >
+        )
     }
 }
 export default Home;
+
+
+
+
+
+
+
+{/* ----------FONT URL---------- */ }
+{/* <style>@import url('https://cloud.typography.com/7268774/6354952/css/fonts.css');</style> */ }
