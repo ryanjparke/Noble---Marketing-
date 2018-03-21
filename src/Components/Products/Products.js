5c2b2eb5404262659a072c1a44f7ddf9aa5bd3db
Ryan Parke <ryanjparke94@gmail.com>
2018-03-21 15:48:26 -0600

    products and assessment desktop views

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -6,7 +6,7 @@ import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 
 import Productsback from './../../assets/Productsback.jpg';
-import Desktop from './../../assets/Desktop.svg';
+import Desktop from './../../assets/Desktop.png';
 
 
 
@@ -33,16 +33,15 @@ class Products extends Component {
                 <Nav />
                 {/************* LEFT SIDE *************/}
                 <div className='productsSideNav'>
-                    <div className='navOverlay'>
-                        <h2>OUR PRODUCTS</h2>
+                    <div className='navHeader'>
+                        <p>OUR PRODUCTS</p>
                     </div>
                     <div className='innerProducts'>
-                        <div className='testButton' onClick={this.toggleModalFunc}>TESTER</div>
-                        <a href='/products'><h3>Assessments</h3> </a>
-                        <a href=''><h3>Training</h3> </a>
-                        <a href=''><h3>Extensions</h3> </a>
-                        <a href=''><h3>Integrations</h3> </a>
-                        <a href=''><h3>Something Else?</h3> </a>
+                        <a href='/assessments'><p>Assessments</p> </a>
+                        <a href=''><p>Training</p> </a>
+                        <a href=''><p>Extensions</p> </a>
+                        <a href=''><p>Integrations</p> </a>
+                        <a href=''><p>Something?</p> </a>
                     </div>
 
                 </div>
