7f10d1ffe8babdbd99bd6a84dd0d24cca683b92b
Ryan Parke <ryanjparke94@gmail.com>
2018-03-27 13:05:51 -0600

    mobile about view

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -24,17 +24,94 @@ class About extends Component {
             <div className='AboutContainer'>
                 <Nav />
                 <div className='aboutHeader' >
-                    <img className='nobleBlue' src={Nobleblue} />
-                    <h1>OUR STORY</h1>
+                    {/* <img className='nobleBlue' src={Nobleblue} /> */}
+                    <p>WE'RE NOBLE</p>
                 </div>
                 <div className="storyContainer" >
-                    <h1>In the beginning</h1>
+                    <p>Noble was founded in the early summer of 2012 by a close-knit team of co-workers who shared a common vision: help improve the way corrections assesses and treats the criminogenic needs of offenders.  Our goal is very simple; we want to have the best assessment and case planning tools in the country.  We do this by maintaining close relationships with assessment authors, using cutting edge software development practices, and most importantly, listening to our users</p>
                 </div>
                 
+                {/************ MOBILE VIEW *************/}
+                <div className='aboutHeader'>
+                    <p>THE TEAM</p>
+                </div>
+                <div className='mobileTeam'>
+                    <div className='member'>
+                        <div className='memberPhoto'>
+                            <p>member photo</p>
+                        </div>
+                        <div className='memberName'>
+                            <p>Diana Norris</p>
+                            <p>President</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='memberPhoto'>
+                            <p>member photo</p>
+                        </div>
+                        <div className='memberName'>
+                            <p>Diana Norris</p>
+                            <p>President</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='memberPhoto'>
+                            <p>member photo</p>
+                        </div>
+                        <div className='memberName'>
+                            <p>Diana Norris</p>
+                            <p>President</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='memberPhoto'>
+                            <p>member photo</p>
+                        </div>
+                        <div className='memberName'>
+                            <p>Diana Norris</p>
+                            <p>President</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='memberPhoto'>
+                            <p>member photo</p>
+                        </div>
+                        <div className='memberName'>
+                            <p>Diana Norris</p>
+                            <p>President</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='memberPhoto'>
+                            <p>member photo</p>
+                        </div>
+                        <div className='memberName'>
+                            <p>Diana Norris</p>
+                            <p>President</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='memberPhoto'>
+                            <p>member photo</p>
+                        </div>
+                        <div className='memberName'>
+                            <p>Diana Norris</p>
+                            <p>President</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='memberPhoto'>
+                            <p>member photo</p>
+                        </div>
+                        <div className='memberName'>
+                            <p>Diana Norris</p>
+                            <p>President</p>
+                        </div>
+                    </div>
 
-
+                </div>
+                {/**** CONTAINER DIV ****/}
                 <Footer />
-
             </div >
         )
     }
